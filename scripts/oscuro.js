8a1b309b891dd268db77524c272c6f91f8277028
Sergioig37 <sergioig37@educastur.es>
2023-12-14 12:59:21 +0100

    muchos cambios

---
+++ scripts/oscuro.js
@@ -0,0 +1,50 @@
+
+window.addEventListener("load", init);
+
+
+
+function init() {
+
+    document.querySelector(".modoOscuro").addEventListener("click", activar);
+    document.querySelector(".hamburguesa").addEventListener("click", mostrar);
+
+}
+
+
+function activar(elemento) {
+    var contenedor = document.querySelector(".contenedor");
+    mover(contenedor);
+    cambiarColor();
+
+}
+
+function mover(contenedor) {
+
+    switch (contenedor.style.justifyContent) {
+        case "flex-start":
+            contenedor.style.justifyContent = "flex-end";
+            break;
+        case "flex-end":
+            contenedor.style.justifyContent = "flex-start";
+            break;
+        default:
+            contenedor.style.justifyContent = "flex-end";
+    }
+
+}
+
+function cambiarColor() {
+
+
+    if(document.body.style.filter =="invert(100%)"){
+        document.body.style.filter ="invert(0%)";
+    }else{
+        document.body.style.filter ="invert(100%)";
+    }
+}
+
+
+function mostrar(){
+    
+}
+
